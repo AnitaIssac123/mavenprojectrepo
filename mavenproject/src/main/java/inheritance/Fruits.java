222cf2fcbb5c823f148d4e7bd8939a391efb06d2
hp <hp@DESKTOP-FD23D29>
2024-03-27 07:45:37 +0530

    hai java

---
+++ mavenproject/src/main/java/inheritance/Fruits.java
@@ -0,0 +1,16 @@
+package inheritance;
+
+public class Fruits {
+
+	
+		
+		public static void show()
+		{
+			System.out.println("hello");		}
+		// TODO Auto-generated method stub
+		public static void main(String[] args) {
+			Fruits.show();
+
+	}
+
+}
